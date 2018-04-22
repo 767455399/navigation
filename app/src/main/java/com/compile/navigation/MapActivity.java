b783159b1098b38fe6afa485b8a54abef89f5244
wangqing <767455399@qq.com>
2018-04-22 18:58:30 +0800

    定位第一步

--- app/src/main/java/com/compile/navigation/MapActivity.java
+++ app/src/main/java/com/compile/navigation/MapActivity.java
@@ -1,34 +1,128 @@
 package com.compile.navigation;
 
 import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import com.amap.api.navi.AMapNaviView;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.navi.AMapNaviViewListener;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.BusRouteResult;
+import com.amap.api.services.route.DriveRouteResult;
+import com.amap.api.services.route.RideRouteResult;
+import com.amap.api.services.route.RouteSearch;
+import com.amap.api.services.route.WalkRouteResult;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements AMapNaviViewListener,AMap.OnMapClickListener, AMap.OnMarkerClickListener, AMap.OnInfoWindowClickListener, AMap.InfoWindowAdapter, RouteSearch.OnRouteSearchListener {
     public static final int ACCESS_FINE_LOCATION_PERMISSION=0;
     private static final String PACKAGE_URL_SCHEME = "package:";
-    private AMapNaviView aMapNaviView;
+    private AMap aMap;
+    private MapView mapView;
+    private Context mContext;
+    private RouteSearch mRouteSearch;
+    private BusRouteResult mBusRouteResult;
+    private LatLonPoint mStartPoint = new LatLonPoint(39.942295, 116.335891);//起点，116.335891,39.942295
+    private LatLonPoint mEndPoint = new LatLonPoint(39.995576, 116.481288);//终点，116.481288,39.995576
+    private String mCurrentCityName = "北京";
+    private final int ROUTE_TYPE_BUS = 1;
+    private LinearLayout mBusResultLayout;
+    private RelativeLayout mHeadLayout;
+    private ListView mBusResultList;
+    private ProgressDialog progDialog = null;// 搜索时进度条
+    private MyLocationStyle myLocationStyle;
+//    LocationSource.OnLocationChangedListener mListener;
+//    AMapLocationClient mlocationClient;
+//    AMapLocationClientOption mLocationOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         requestPermissions();
+        mContext = this.getApplicationContext();
+        mapView = (MapView) findViewById(R.id.route_map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
         initView();
+        setfromandtoMarker();
+        mapView.setVisibility(View.GONE);
+        mBusResultLayout.setVisibility(View.VISIBLE);
+        searchRouteResult(ROUTE_TYPE_BUS, RouteSearch.BusDefault);
     }
 
     private void initView() {
-        aMapNaviView=(AMapNaviView)findViewById(R.id.aMapNaviView);
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        registerListener();
+        mRouteSearch = new RouteSearch(this);
+        mRouteSearch.setRouteSearchListener(this);
+        mBusResultLayout = (LinearLayout) findViewById(R.id.bus_result);
+        mBusResultList = (ListView) findViewById(R.id.bus_result_list);
+        mHeadLayout = (RelativeLayout) findViewById(R.id.routemap_header);
+        mHeadLayout.setVisibility(View.GONE);
+    }
+
+    /**
+     * 注册监听
+     */
+    private void registerListener() {
+        aMap.setOnMapClickListener(MapActivity.this);
+        aMap.setOnMarkerClickListener(MapActivity.this);
+        aMap.setOnInfoWindowClickListener(MapActivity.this);
+        aMap.setInfoWindowAdapter(MapActivity.this);
+
+    }
+
+    /**
+     * 开始搜索路径规划方案
+     */
+    public void searchRouteResult(int routeType, int mode) {
+        if (mStartPoint == null) {
+            Toast.makeText(mContext, "定位中，稍后再试...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mEndPoint == null) {
+            Toast.makeText(mContext, "终点未设置", Toast.LENGTH_SHORT).show();
+        }
+        showProgressDialog();
+        final RouteSearch.FromAndTo fromAndTo = new RouteSearch.FromAndTo(
+                mStartPoint, mEndPoint);
+        if (routeType == ROUTE_TYPE_BUS) {// 公交路径规划
+            RouteSearch.BusRouteQuery query = new RouteSearch.BusRouteQuery(fromAndTo, mode,
+                    mCurrentCityName, 0);// 第一个参数表示路径规划的起点和终点，第二个参数表示公交查询模式，第三个参数表示公交查询城市区号，第四个参数表示是否计算夜班车，0表示不计算
+            mRouteSearch.calculateBusRouteAsyn(query);// 异步路径规划公交模式查询
+        }
+    }
+
+    private void setfromandtoMarker() {
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mStartPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.start)));
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mEndPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.end)));
     }
 
 
@@ -59,5 +153,164 @@ public class MapActivity extends AppCompatActivity {
                 dialog.show();
             }
         }
+    }
+
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onNaviSetting() {
+
+    }
+
+    @Override
+    public void onNaviCancel() {
+
+    }
+
+    @Override
+    public boolean onNaviBackClick() {
+        return false;
+    }
+
+    @Override
+    public void onNaviMapMode(int i) {
+
+    }
+
+    @Override
+    public void onNaviTurnClick() {
+
+    }
+
+    @Override
+    public void onNextRoadClick() {
+
+    }
+
+    @Override
+    public void onScanViewButtonClick() {
+
+    }
+
+    @Override
+    public void onLockMap(boolean b) {
+
+    }
+
+    @Override
+    public void onNaviViewLoaded() {
+
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    /**
+     * 规划路线结果回调方法
+     */
+    @Override
+    public void onBusRouteSearched(BusRouteResult result, int errorCode) {
+        dissmissProgressDialog();
+        aMap.clear();// 清理地图上的所有覆盖物
+        if (errorCode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getPaths() != null) {
+                if (result.getPaths().size() > 0) {
+                    mBusRouteResult = result;
+                    BusResultListAdapter mBusResultListAdapter = new BusResultListAdapter(mContext, mBusRouteResult);
+                    mBusResultList.setAdapter(mBusResultListAdapter);
+                } else if (result != null && result.getPaths() == null) {
+                    Toast.makeText(mContext, getResources().getString(R.string.no_result), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(mContext, getResources().getString(R.string.no_result), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(mContext, errorCode+"", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 显示进度框
+     */
+    private void showProgressDialog() {
+        if (progDialog == null)
+            progDialog = new ProgressDialog(this);
+        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDialog.setIndeterminate(false);
+        progDialog.setCancelable(true);
+        progDialog.setMessage("正在搜索");
+        progDialog.show();
+    }
+
+    /**
+     * 隐藏进度框
+     */
+    private void dissmissProgressDialog() {
+        if (progDialog != null) {
+            progDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onDriveRouteSearched(DriveRouteResult driveRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onRideRouteSearched(RideRouteResult rideRouteResult, int i) {
+
     }
 }
